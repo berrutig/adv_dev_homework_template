2da95cfabdf27be234201d36c09c1edb108bdba0
uzugic <42067801+uzugic@users.noreply.github.com>
2018-09-27 14:20:10 +0200

    Update setup_sonar.sh

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -7,6 +7,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
+oc project ${GUID}-sonarqube
 echo "Setting up Sonarqube in project $GUID-sonarqube"
 
 # Code to set up the SonarQube project.
@@ -14,8 +15,6 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
-
-oc project ${GUID}-sonarqube
 
 oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
 

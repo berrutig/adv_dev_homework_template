1c7c00ed890948655b7728668eb04cfa9cd38dd1
uzugic <42067801+uzugic@users.noreply.github.com>
2018-09-27 14:39:55 +0200

    Update setup_nexus.sh

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -34,7 +34,6 @@ echo "Setting up Nexus in project $GUID-nexus"
 
 
 oc new-app sonatype/nexus3:latest
-sleep 10
 oc expose svc nexus3
 oc rollout pause dc nexus3
 
@@ -57,6 +56,9 @@ oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 
 oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/ 
 oc rollout resume dc nexus3
 
+oc expose dc nexus3 --port=5000 --name=nexus-registry
+oc create route edge nexus-registry --service=nexus-registry --port=5000
+
 while : ; do
   echo "Checking if Nexus is Ready..."
   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
@@ -69,8 +71,7 @@ curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advan
 chmod +x setup_nexus3.sh
 ./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
 rm setup_nexus3.sh 
-oc expose dc nexus3 --port=5000 --name=nexus-registry
-oc create route edge nexus-registry --service=nexus-registry --port=5000
+
 
 
 
